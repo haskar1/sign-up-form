82fa870f6c9b7041daec1430f362294dfce9e5d1
haskar1 <94493175+haskar1@users.noreply.github.com>
2022-06-03 14:28:33 -0400

    Add checkmark next to first password

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const createAccountBtn = document.querySelector('.account-button button');
 const requiredFields = document.querySelectorAll('input[required]');
+const firstPassword = document.getElementById('pass');
+const firstPasswordLabel = document.querySelector('.pass-one-label');
+const secondPassword = document.getElementById('pass2');
+const secondPasswordLabel = document.querySelector('.pass-two-label');
 
 createAccountBtn.addEventListener('click', () => {
     for (let i = 0; i < requiredFields.length; i++) {
@@ -20,3 +24,14 @@ for (let i = 0; i < requiredFields.length; i++) {
         e.target.style.outline = '';
     })
 }
+
+firstPassword.addEventListener('blur', (e) => {
+    if (firstPasswordLabel.classList.contains("checkmark") == true) {
+        firstPasswordLabel.classList.remove("checkmark");
+        console.log('removed checkmark class');
+    }
+    if (e.target.value !== '') {
+        firstPasswordLabel.classList.add("checkmark");
+        console.log('added checkmark class');
+    }
+})
