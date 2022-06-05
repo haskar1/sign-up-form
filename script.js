135e77c414c9be0a7ef02f45d6a068d89435c417
Hussein <gimble20@yahoo.com>
2022-06-04 22:36:53 -0400

    Finish layout and add password check

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const createAccountBtn = document.querySelector('.account-button button');
 const requiredFields = document.querySelectorAll('input[required]');
 
-createAccountBtn.addEventListener('click', () => {
-    for (let i = 0; i < requiredFields.length; i++) {
-        if (requiredFields[i].value === '') {
-            requiredFields[i].style = "outline: 1px solid red";
-        }
-    }
-})
+const firstPassword = document.getElementById('pass');
+const passOneCheckmark = document.querySelector('.pass-one-checkmark');
+const passOneErrorMsg = document.querySelector('.pass-one-error-msg');
+
+const secondPassword = document.getElementById('pass2');
+const passTwoCheckmark = document.querySelector('.pass-two-checkmark');
+const passTwoErrorMsg = document.querySelector('.pass-two-error-msg');
+
 
 for (let i = 0; i < requiredFields.length; i++) {
     requiredFields[i].addEventListener('input', () => {
@@ -17,6 +18,102 @@ for (let i = 0; i < requiredFields.length; i++) {
     })
 
     requiredFields[i].addEventListener('blur', (e) => {
-        e.target.style.outline = '';
+        if (requiredFields[i].style.outline.includes("blue")) {
+            e.target.style.outline = '';
+        }
     })
 }
+
+
+let buttonClickCheck = 0;
+
+createAccountBtn.addEventListener('click', () => {
+    for (let i = 0; i < requiredFields.length; i++) {
+        if (requiredFields[i].value === '') {
+            requiredFields[i].style = "outline: 1px solid red";
+        }
+    }
+
+    if (passOneCheckmark.style.display === "inline-block" && passTwoCheckmark.style.display === "inline-block") {
+        createAccountBtn.setAttribute("type", "submit");
+        return
+    }
+
+    if (passOneCheckmark.style.display === "none") {
+        createAccountBtn.setAttribute("type", "button");
+        passOneErrorMsg.style = "visibility: visible; color:red;";
+    }
+
+    if (passTwoCheckmark.style.display === "none") {
+        createAccountBtn.setAttribute("type", "button");
+        passTwoErrorMsg.style = "visibility: visible; color:red;";
+    }
+
+    buttonClickCheck = 1;
+})
+
+
+firstPassword.addEventListener('focus', () => {
+    if (buttonClickCheck === 0 && firstPassword.value.length < 6) {
+        passOneErrorMsg.style = "visibility: visible; color: black;";
+    }
+})
+
+firstPassword.addEventListener('blur', () => {
+    if (buttonClickCheck === 0 && firstPassword.value.length === 0) {
+        passOneErrorMsg.style = "visibility: hidden";
+    }
+})
+
+firstPassword.addEventListener('input', () => {
+    if (firstPassword.value.length >= 6) {
+        passOneCheckmark.style.visibility = "visible";
+        passOneErrorMsg.style = "visibility: hidden";
+    } 
+    else if (buttonClickCheck === 1) {
+        passOneCheckmark.style.visibility = "hidden";
+        passOneErrorMsg.style = "visibility: visible; color: red;";
+    }
+    else {
+        passOneCheckmark.style.visibility = "hidden";
+        passOneErrorMsg.style = "visibility: visible; color: black;";
+    }
+
+    if (firstPassword.value !== secondPassword.value && secondPassword.value !== '') {
+        passTwoCheckmark.style.visibility = "hidden";
+        passTwoErrorMsg.style = "visibility: visible; color:black;";        
+    }
+    
+    if (firstPassword.value !== secondPassword.value && secondPassword.value !== '' && buttonClickCheck === 1) {
+        passTwoErrorMsg.style = "visibility: visible; color:red;";        
+    }
+    
+    if (firstPassword.value === secondPassword.value && secondPassword.value !== '') {
+        passTwoCheckmark.style.visibility = "visible";
+        passTwoErrorMsg.style = "visibility: hidden";           
+    }
+})
+
+
+
+secondPassword.addEventListener('input', () => { 
+    if (firstPassword.value === '' && buttonClickCheck === 0 
+    || secondPassword.value === '' && buttonClickCheck === 0) {
+
+        passTwoErrorMsg.style = "visibility: hidden";
+        return
+    }
+
+    if (firstPassword.value === secondPassword.value) {
+        passTwoCheckmark.style.visibility = "visible";
+        passTwoErrorMsg.style = "visibility: hidden";
+    } 
+    else if (firstPassword.value !== secondPassword.value && buttonClickCheck === 1) {
+        passTwoCheckmark.style.visibility = "hidden";
+        passTwoErrorMsg.style = "visibility: visible; color:red;";    
+    }
+    else {
+        passTwoCheckmark.style.visibility = "hidden";
+        passTwoErrorMsg.style = "visibility: visible; color:black;";
+    }
+})
